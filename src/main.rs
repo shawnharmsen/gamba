fd226e5d51b071fa2d7cc8331157a04b9cbe3087
Shawn Harmsen <shawn@zksnarks.xyz>
2023-05-14 22:59:59 +0900

    test working

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
-mod app;
+use yew::prelude::*;
 
-use app::App;
+#[function_component]
+fn App() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
+
+    html! {
+        <div>
+            <button {onclick}>{ "+1" }</button>
+            <p>{ *counter }</p>
+        </div>
+    }
+}
 
 fn main() {
     yew::Renderer::<App>::new().render();
